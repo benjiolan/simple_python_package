c096953b2170a7efbdb5fa5d61df02d5a8660230
benjiolan <benji264@yahoo.com>
2024-08-12 01:50:27 -0600

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# simple_python_package
-Creating a custom Python package and submitting to pypi
+# Example Package
+
+This is a simple example package. You can use
+[GitHub-flavored Markdown](https://guides.github.com/features/mastering-markdown/) to write your content.
---
+++ src/utilsbenolan/utils.py
@@ -0,0 +1,23 @@
+saludo = "Este es un saludo inicial"
+msg = "Este es un mensaje de prueba"
+despedida = "Este es un mensaje de despedida"
+
+def sumar(a, b):
+    """Suma dos números enteros"""
+    return a + b
+
+
+def restar(a, b):
+    """Resta dos números enteros"""
+    return a - b
+
+def multiplicar(a, b): 
+    """Multiplica dos números enteros"""
+    return a * b
+def dividir(a, b):
+    """Divide dos números enteros"""
+    return a / b
+
+def inicio(msg):
+    """Imprime un mensaje de inicio"""
+    print(msg)
